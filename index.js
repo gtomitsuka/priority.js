eaab84cc7df0f888b513817953e363b9bdc3a0c5
Gabriel Tomitsuka <gabriel.tomitsuka@montreus.com>
2015-02-28 00:16:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 /* priority.js - index.js
  */
+
+var EventEmitter = require('events').EventEmitter; //For emmiting events
+var priority = new EventEmitter();
+var util = require('util');
+
+function Priority(initial) {
+	
+	var queue = [];
+	var self = this;
+	queue = initial; //On start, the queue will be the initial values
+	
+	this.on('queue', function(object){ queue.push(object) });
+	this.size = function(){ return queue.length()};
+	
+	var first = queue.sort(compare)[0];
+	self.emit('dequeue', first);
+	queue.shift();
+	
+	this.on('dequeueRequest', function(){
+		var next = queue.sort(compare)[0];
+		self.emit('dequeue', next);
+		queue.shift();
+	});
+}
+
+function compare(a,b) {
+  if (a.priority < b.priority)
+     return -1;
+  if (a.priority > b.priority)
+    return 1;
+  return 0;
+}
+
+module.exports = Priority;
